14f378b1242ecc60551b975d9530b88f91a21022
Amjad Ali <qatarseo9492@gmail.com>
2025-08-27 14:35:43 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
-// routes/api.php  (or a file included by your RouteServiceProvider)
 use App\Http\Controllers\ContentDetectionController;
 
-Route::post('/detect', [ContentDetectionController::class, 'detect']);      // returns JSON
-Route::post('/detect/url', [ContentDetectionController::class, 'detectUrl']); // returns JSON
+Route::post('/detect',     [ContentDetectionController::class, 'detect']);      // JSON
+Route::post('/detect/url', [ContentDetectionController::class, 'detectUrl']);   // JSON
