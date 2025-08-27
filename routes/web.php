a860836ce06e6ea9e7cf36e9483fcd857ec019fe
Amjad Ali <qatarseo9492@gmail.com>
2025-08-27 16:48:10 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Seo\TopicAnalysisController;
-use App\Models\TopicAnalysis;
 
-// Topic Cluster Analyzer
-Route::get('/seo/topic-clusters', [TopicAnalysisController::class, 'create'])
-    ->name('seo.topic-clusters.create');
-
-Route::post('/seo/topic-clusters', [TopicAnalysisController::class, 'store'])
-    ->name('seo.topic-clusters.store');
-
-// Results page via simple route-model binding (keeps controller with only 2 methods)
-Route::get('/seo/topic-clusters/results/{analysis}', function (TopicAnalysis $analysis) {
-    return view('seo.topic-analysis-results', ['analysis' => $analysis]);
-})->name('seo.topic-clusters.results');
+// Home page -> resources/views/home.blade.php
+Route::view('/', 'home')->name('home');
