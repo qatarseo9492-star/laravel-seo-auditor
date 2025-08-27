1b7a7279b2e23d67c45f4cad3746e40a8feb2885
Amjad Ali <qatarseo9492@gmail.com>
2025-08-27 16:55:17 +0500

    Add routes for Topic Cluster Analyzer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Seo\TopicAnalysisController;
+use App\Models\TopicAnalysis;
 
-// Home page -> resources/views/home.blade.php
+// Home -> resources/views/home.blade.php
 Route::view('/', 'home')->name('home');
+
+// Topic Cluster Analyzer (GET form + POST process)
+Route::get('/seo/topic-clusters', [TopicAnalysisController::class, 'create'])
+    ->name('seo.topic-clusters.create');
+
+Route::post('/seo/topic-clusters', [TopicAnalysisController::class, 'store'])
+    ->name('seo.topic-clusters.store');
+
+// Results page
+Route::get('/seo/topic-clusters/results/{analysis}', function (TopicAnalysis $analysis) {
+    return view('seo.topic-analysis-results', ['analysis' => $analysis]);
+})->name('seo.topic-clusters.results');
