f424f7e9c840dfaf5cd1eeb504abf3a21802aeb8
Amjad Ali <qatarseo9492@gmail.com>
2025-08-27 14:41:58 +0500

    Delete app/Providers/RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++
@@ -1,9 +0,0 @@
-// app/Providers/RouteServiceProvider.php
-public function boot(): void
-{
-    parent::boot();
-
-    \Illuminate\Support\Facades\Route::middleware('api')
-        ->prefix('api')
-        ->group(base_path('routes/api-detection.php'));
-}
