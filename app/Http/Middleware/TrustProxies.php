d981b095597b01f9d4374dbd090ddf43ef74c19f
Amjad Ali <qatarseo9492@gmail.com>
2025-08-28 12:55:14 +0500

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,24 +3,29 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-// Use Symfony's Request constants for forwarded headers
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class TrustProxies extends Middleware
 {
     /**
-     * Trust all upstream proxies (Cloudflare/Nginx/Load balancers).
-     * If you prefer, list specific IPs/CIDRs instead of '*'.
+     * The trusted proxies for this application.
+     *
+     * Use '*' to trust all (Cloudflare/Nginx/ELB). You can also
+     * provide an array of IPs/CIDRs if you want to be explicit.
+     *
+     * @var array<int, string>|string|null
      */
     protected $proxies = '*';
 
     /**
-     * Tell Laravel which X-Forwarded-* headers to honor.
-     * Equivalent to HEADER_X_FORWARDED_ALL (bitmask 1|2|4|8 = 15).
+     * The headers that should be used to detect proxies.
+     * Equivalent to HEADER_X_FORWARDED_ALL.
+     *
+     * @var int
      */
     protected $headers =
-          SymfonyRequest::HEADER_X_FORWARDED_FOR
-        | SymfonyRequest::HEADER_X_FORWARDED_HOST
-        | SymfonyRequest::HEADER_X_FORWARDED_PROTO
-        | SymfonyRequest::HEADER_X_FORWARDED_PORT;
+        SymfonyRequest::HEADER_X_FORWARDED_FOR |
+        SymfonyRequest::HEADER_X_FORWARDED_HOST |
+        SymfonyRequest::HEADER_X_FORWARDED_PORT |
+        SymfonyRequest::HEADER_X_FORWARDED_PROTO;
 }
