ab8e0cfc0de28454787b2c61aa346ef71f2c38d4
Amjad Ali <qatarseo9492@gmail.com>
2025-09-03 00:22:32 +0500

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title','Admin — Dashboard')
-
+<script src="{{ asset('js/admin-dashboard.js') }}" defer></script>
 @push('head')
 <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 
